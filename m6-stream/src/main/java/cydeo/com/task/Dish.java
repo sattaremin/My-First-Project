ecc266c2bee4a758032b2cb0e27a523f710e1744
sattar <m.sallihemin14@gmail.com>
2024-05-22 12:01:06 -0400

    Stream

---
+++ m6-stream/src/main/java/cydeo/com/task/Dish.java
@@ -0,0 +1,17 @@
+package cydeo.com.task;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Dish {
+
+
+    private String name;
+    private boolean vegetarian;
+    private int calories;
+    private Type type;
+}
+
---
+++ m6-stream/src/main/java/cydeo/com/task/Employee.java
@@ -0,0 +1,19 @@
+package cydeo.com.task;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+public class Employee {
+
+
+    private int empId;  //.stream  Stream<>
+    private String empName;
+    private String empEmail;
+    private List<String> empPhoneNumbers; //.stream Stream<
+
+
+
+}
